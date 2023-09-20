a7e19e4c0af9ee14020fcbe1e90e368ec9f35021
YakupAltay <yakup.altay@boun.edu.tr>
2023-09-20 22:00:56 +0300

    backend integration

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const bodyParser = require('body-parser');
 const cluster = require('cluster');
+const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const express = require('express');
 const favicon = require('serve-favicon');
 const http = require('http');
+const i18n = require('i18n');
+const mongoose = require('mongoose');
 const path = require('path');
+const session = require('express-session');
+
+const MongoStore = require('connect-mongo');
 
 dotenv.config({ path: path.join(__dirname, '.env') });
 const numCPUs = process.env.WEB_CONCURRENCY || require('os').cpus().length;
@@ -25,13 +31,30 @@ if (cluster.isMaster) {
 
   const MAX_SERVER_UPLOAD_LIMIT = 52428800;
   const MAX_SERVER_PARAMETER_LIMIT = 50000;
+  const QUERY_LIMIT = 20;
   const PORT = process.env.PORT || 3000;
   const URL = process.env.URL || 'http://localhost:3000';
+  const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/node101';
 
   const indexRouteController = require('./routes/indexRoute');
+  const guideRouteController = require('./routes/guideRoute');
+  const venueRouteController = require('./routes/venueRoute');
 
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'pug');
+
+  mongoose.set('strictQuery', false);
+  mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  i18n.configure({
+    locales:['tr', 'en'],
+    directory: __dirname + '/translations',
+    queryParameter: 'lang',
+    defaultLocale: 'tr'
+  });
 
   app.use(express.static(path.join(__dirname, 'public')));
   app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -41,19 +64,39 @@ if (cluster.isMaster) {
     limit: MAX_SERVER_UPLOAD_LIMIT,
     parameter: MAX_SERVER_PARAMETER_LIMIT
   }));
+  app.use(i18n.init);
+
+  const sessionOptions = session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: MONGODB_URI
+    })
+  });
+
+  app.use(cookieParser());
+  app.use(sessionOptions);
 
   app.use((req, res, next) => {
-    res.locals.URL = URL;
-
     if (!req.query || typeof req.query != 'object')
       req.query = {};
     if (!req.body || typeof req.body != 'object')
       req.body = {};
+    if (!req.session || typeof req.session != 'object')
+      req.session = {};
+
+    res.locals.URL = URL;
+    res.locals.QUERY_LIMIT = QUERY_LIMIT;
+    req.query.limit = QUERY_LIMIT;
+    req.body.limit = QUERY_LIMIT;
 
     next();
   });
 
   app.use('/', indexRouteController);
+  app.use('/guide', guideRouteController);
+  app.use('/venue', venueRouteController);
 
   server.listen(PORT, () => {
     console.log(`Server is on port ${PORT} as Worker ${cluster.worker.id} running @ process ${cluster.worker.process.pid}`);
