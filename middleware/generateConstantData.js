17c54735b1fdccb1c0373062c9e1f415ebadbbb0
YakupAltay <yakup.altay@boun.edu.tr>
2023-09-20 22:02:48 +0300

    modified middleware

--- middleware/generateConstantData.js
+++ middleware/generateConstantData.js
@@ -11,6 +11,67 @@ module.exports = (req, res, next) => {
   if (!language || !ALLOWED_LANGUAGE_VALUES.includes(language))
     language = DEFAULT_LANGUAGE;
 
+  const EVENT_TYPES = [
+    res.__('Summit'),
+    res.__('Party'),
+    res.__('Conference'),
+    res.__('Hackathon'),
+    res.__('Meetup'),
+    res.__('Workshop'),
+    res.__('Dinner'),
+    res.__('Brunch'),
+    res.__('Co-living'),
+    res.__('Co-work'),
+    res.__('NFTs'),
+    res.__('Tour'),
+    res.__('Other')
+  ];
+  const FULL_MONTHS = [
+    res.__('January'),
+    res.__('February'),
+    res.__('March'),
+    res.__('April'),
+    res.__('May'),
+    res.__('June'),
+    res.__('July'),
+    res.__('August'),
+    res.__('September'),
+    res.__('October'),
+    res.__('November'),
+    res.__('December')
+  ];
+  const SHORT_MONTHS = [
+    res.__('Jan'),
+    res.__('Feb'),
+    res.__('Mar'),
+    res.__('Apr'),
+    res.__('May'),
+    res.__('Jun'),
+    res.__('Jul'),
+    res.__('Aug'),
+    res.__('Sep'),
+    res.__('Oct'),
+    res.__('Nov'),
+    res.__('Dec')
+  ];
+
+  const DISTRICTS = [
+    res.__('Historical Peninsula (Fatih)'),
+    res.__('Historical European District (Beyoğlu)'),
+    res.__('Şişli'),
+    res.__('Beşiktaş'),
+    res.__('Sarıyer'),
+    res.__('Golden Horn (Haliç)'),
+    res.__('Kadıköy'),
+    res.__('Üsküdar'),
+    res.__('Beykoz'),
+    res.__('Adalar')
+  ]
+
+  res.locals.EVENT_TYPES = EVENT_TYPES;
+  res.locals.FULL_MONTHS = FULL_MONTHS;
+  res.locals.SHORT_MONTHS = SHORT_MONTHS;
+  res.locals.DISTRICTS = DISTRICTS;
   res.locals.query_lang = query_language;
   res.locals.lang = language;
 
